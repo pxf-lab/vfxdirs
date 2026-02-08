7a498907bd82e20017f2741f3ce89bfb53449069
Ben Cressman <ben@utdallas.edu>
2026-02-08 11:28:47 -0600

    feat: make src a package and export public api

--- src/vfxdirs/__init__.py
+++ src/vfxdirs/__init__.py
@@ -1,2 +1,28 @@
-def main() -> None:
-    print("Hello from vfxdirs!")
+"""vfxdirs public API."""
+
+from .api import AppDirs, VFXApp, VFXDirs, path
+from .config import (
+    AppConfig,
+    InstallOverride,
+    VFXDirsConfig,
+    VFXDirsConfigError,
+    default_config_path,
+)
+from .context import Context
+from .keys import DirKey, KeyLike
+
+__all__ = [
+    "AppDirs",
+    "AppConfig",
+    "Context",
+    "InstallOverride",
+    "DirKey",
+    "KeyLike",
+    "VFXDirsConfig",
+    "VFXDirsConfigError",
+    "VFXApp",
+    "VFXDirs",
+    "default_config_path",
+    "main",
+    "path",
+]
